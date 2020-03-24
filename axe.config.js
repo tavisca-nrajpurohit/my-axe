7affb5a41f288cc4b368fdba26e08d60f079f747
Neelesh Rajpurohit <nrajpurohit@tavisca.com>
2020-03-24 10:09:00 +0530

    updated package

--- axe.config.js
+++
@@ -1,8 +0,0 @@
-const DEFAULT_AXE_CONFIG = {
-    "runOnly": {
-      "type": "tags",
-      "values": ["wcag2a", "wcag2aa","section508","best-practice"]
-    }
-  };
-
-  module.exports = DEFAULT_AXE_CONFIG;
